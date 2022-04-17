6591d1a83724665759b8167ae6dd5d7e5cc7ec99
riolite <riolite@live.com>
2022-04-18 00:15:50 +0300

    refactors CreateBook to func, rename component name, and rename once book is submitted

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import CreateBook from "./components/CreateBook";
+import CreateBook from "./components/CreateBookTest";
 import ShowBookList from "./components/ShowBookList";
 import ShowBookDetails from "./components/ShowBookDetails";
 import UpdateBookInfo from "./components/UpdateBookInfo";
---
+++ client/src/components/CreateBookTest.js
@@ -0,0 +1,141 @@
+import React, { Component, useState } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import axios from "axios";
+import { createHashHistory } from "history";
+export const history = createHashHistory();
+const CreateBook = () => {
+  const [book, setBook] = useState({
+    title: "",
+    isbn: "",
+    author: "",
+    description: "",
+    published_date: "",
+    publisher: "",
+  });
+
+  // onChange = (e) => {
+  //   this.setState({ [e.target.name]: e.target.value });
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = book;
+
+    axios
+      .post("http://localhost:8002/api/books", data)
+      .then((res) => {
+        setBook({
+          title: "",
+          isbn: "",
+          author: "",
+          description: "",
+          published_date: "",
+          publisher: "",
+        });
+        //redirect to homepage
+      })
+      .catch((err) => {
+        console.log("Error in CreateBook!");
+      });
+  };
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setBook((values) => ({ ...values, [name]: value }));
+  };
+
+  return (
+    <div className="CreateBook">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <br />
+            <Link to="/" className="btn btn-outline-warning float-left">
+              Show BooK List
+            </Link>
+          </div>
+          <div className="col-md-8 m-auto">
+            <h1 className="display-4 text-center">Add Book</h1>
+            <p className="lead text-center">Create new book</p>
+
+            <form noValidate onSubmit={handleSubmit}>
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Title of the Book"
+                  name="title"
+                  className="form-control"
+                  value={book.title}
+                  onChange={handleChange}
+                />
+              </div>
+              <br />
+
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="ISBN"
+                  name="isbn"
+                  className="form-control"
+                  value={book.isbn}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Author"
+                  name="author"
+                  className="form-control"
+                  value={book.author}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Describe this book"
+                  name="description"
+                  className="form-control"
+                  value={book.description}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <input
+                  type="date"
+                  placeholder="published_date"
+                  name="published_date"
+                  className="form-control"
+                  value={book.published_date}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Publisher of this Book"
+                  name="publisher"
+                  className="form-control"
+                  value={book.publisher}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <input
+                type="submit"
+                className="btn btn-outline-warning btn-block mt-4"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateBook;
